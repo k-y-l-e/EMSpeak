7b1797f6dc23ca4a99152b202bd86ba64912ba78
Kyle Kable <kablek@wit.edu>
2018-05-21 23:15:18 -0400

    Added button array and onclicklisteners.

--- app/src/main/java/edu/wit/seniorproject/emspeak/emspeak/MainActivity.java
+++ app/src/main/java/edu/wit/seniorproject/emspeak/emspeak/MainActivity.java
@@ -13,13 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     LinearLayout btnLin;
+    EditText et;
+    TextView tv;
+
 
 
     @Override
@@ -31,15 +36,69 @@ public class MainActivity extends AppCompatActivity
 
 
 
-
+        /*
+        // test, programmatically generate buttons
         btnLin=(LinearLayout)findViewById(R.id.btnSV);
-
         Button[] btn = new Button[10];
         for (int i = 0; i < 10; i++) {
             btn[i] = new Button(this);
             //btn[i].setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
             btn[i].setText("This is the button" + i);
             btnLin.addView(btn[i]);
+        } */
+
+
+
+
+        /* Programmatically create buttons based off quickBtn array */
+
+        String quickBtn[] = {"Do you speak English?",
+                            "What is your name?",
+                            "Are you under the influence of drugs or alcohol?",
+                            "Show me where the pain is.",
+                            "Is there anyone else with you?",
+                            "Can you feel this?",
+                            "What is your blood type?",
+                            "Do you know where you are?",
+                            "Do you remember what happened?",
+                            "Do you have any allergies?",
+                            "Are you taking any medication?",
+                            "Are you having difficulty breathing?",
+                            "How does your head feel?",
+                            "Is your vision normal?"};
+
+
+        btnLin=(LinearLayout)findViewById(R.id.btnSV);
+        et = (EditText) findViewById(R.id.inputTxt);
+        tv = (TextView) findViewById(R.id.outputTxt);
+
+        int n = quickBtn.length;
+
+        Button[] btn = new Button[n];
+
+        for (int i = 0; i < n; i++) {
+            btn[i] = new Button(this); //create button object
+            btn[i].setText(quickBtn[i]); //sets button's text
+            btnLin.addView(btn[i]); //adds button to LinearLayout inside ScrollView
+
+
+            /* creates onClickListener for each button so when clicked,
+               its text will populate the text fields */
+            btn[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Button b = (Button)v;
+                    String bTxt = b.getText().toString();
+
+                    et.setText(bTxt);
+                    tv.setText(bTxt);
+
+                }
+            });
+
+
+
         }
 
 
