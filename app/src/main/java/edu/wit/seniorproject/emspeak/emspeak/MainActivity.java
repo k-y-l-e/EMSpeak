23308fc9d9ff57155cca3b4384602dad8423fd32
k-y-l-e <36251364+k-y-l-e@users.noreply.github.com>
2018-06-11 09:49:37 -0400

    Manually added changes.
    Commits through Android Studio are acting up.

--- app/src/main/java/edu/wit/seniorproject/emspeak/emspeak/MainActivity.java
+++ app/src/main/java/edu/wit/seniorproject/emspeak/emspeak/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.wit.seniorproject.emspeak.emspeak;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,16 +17,140 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
+
+import android.os.AsyncTask;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     LinearLayout btnLin;
     EditText et;
     TextView tv;
+    ImageButton playBtn;
+
+
+
+
+
+
+
+
+    // --------------------------------------------------------------------
+    // All the microsoft info we need
+
+    static String subscriptionKey = "cbb3735223de414aae6bc3c5885e8630";
+
+    static String host = "https://api.cognitive.microsofttranslator.com";
+    static String path = "/translate?api-version=3.0";
+
+    // Translate to German and Italian.
+    static String params = "&to=es";//&to=it";
+    static String text;// = "Hello my name is Kyle";
+    // --------------------------------------------------------------------
+
+
+
+
+
+
+
+    // --------------------------------------------------------------------
+    // all of Microsofts required methods
+
+
+    public static class RequestBody {
+        String Text;
+
+        public RequestBody(String text) {
+            this.Text = text;
+        }
+    }
+
+    public static String Post (URL url, String content) throws Exception {
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Content-Length", content.length() + "");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+        connection.setRequestProperty("X-ClientTraceId", java.util.UUID.randomUUID().toString());
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        byte[] encoded_content = content.getBytes("UTF-8");
+        wr.write(encoded_content, 0, encoded_content.length);
+        wr.flush();
+        wr.close();
+
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    public static String Translate () throws Exception {
+
+        URL url = new URL (host + path + params);
+
+
+        List<RequestBody> objList = new ArrayList<RequestBody>();
+        objList.add(new RequestBody(text));
+        String content = new Gson().toJson(objList);
+
+        return Post(url, content);
+    }
+
+    // unnecessary, puts json in newline format
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+
+    // parse json string to only get translated text
+    public static String getTranslatedText(String json_text) {
+        String str00 = json_text;
+        String [] arrOfStr00 = str00.split("text");
+        String [] arrOfStr001 = arrOfStr00[1].split(",");
+        String [] arrOfStr002 = arrOfStr001[0].split(":");
+        //return arrOfStr002[1]; //includes quotes but not too important to worry about
+
+        String transdTxt = arrOfStr002[1].replace("\"", "");
+        return transdTxt;
+
+    }
+
+
+    // --------------------------------------------------------------------
+
 
 
 
@@ -37,17 +163,39 @@ public class MainActivity extends AppCompatActivity
 
 
 
-        /*
-        // test, programmatically generate buttons
-        btnLin=(LinearLayout)findViewById(R.id.btnSV);
-        Button[] btn = new Button[10];
-        for (int i = 0; i < 10; i++) {
-            btn[i] = new Button(this);
-            //btn[i].setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-            btn[i].setText("This is the button" + i);
-            btnLin.addView(btn[i]);
-        } */
 
+
+
+        btnLin=(LinearLayout)findViewById(R.id.btnSV);
+        et = (EditText) findViewById(R.id.inputTxt);
+        tv = (TextView) findViewById(R.id.outputTxt);
+
+
+
+
+
+        /*
+            this was placed inside the button creation so each button
+            will make call for translation
+         */
+        /*
+        // cannot run on main thread so run as async task
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    String response = Translate();
+                    tv.setText(response);
+
+                } catch (Exception e) {
+                    Log.e("TAG", String.valueOf(e));
+                }
+
+            }
+
+        });
+        */
 
 
 
@@ -69,19 +217,26 @@ public class MainActivity extends AppCompatActivity
                             "Is your vision normal?"};
 
 
+
+
+
+
+
+
         btnLin=(LinearLayout)findViewById(R.id.btnSV);
         et = (EditText) findViewById(R.id.inputTxt);
         tv = (TextView) findViewById(R.id.outputTxt);
+
 
         int n = quickBtn.length;
 
         Button[] btn = new Button[n];
 
+
         for (int i = 0; i < n; i++) {
             btn[i] = new Button(this); //create button object
             btn[i].setText(quickBtn[i]); //sets button's text
             btnLin.addView(btn[i]); //adds button to LinearLayout inside ScrollView
-
 
             /* creates onClickListener for each button so when clicked,
                its text will populate the text fields */
@@ -92,16 +247,95 @@ public class MainActivity extends AppCompatActivity
                     Button b = (Button)v;
                     String bTxt = b.getText().toString();
 
+                    // change variable globally
+                    text = bTxt;
+
                     et.setText(bTxt);
-                    tv.setText(bTxt);
+                    //tv.setText(bTxt);
+
+
+                    AsyncTask.execute(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+                                String response = Translate();
+                                //tv.setText(response); //prints stringified json
+                                tv.setText(getTranslatedText(response));
+
+
+                            } catch (Exception e) {
+                                Log.e("TAG", String.valueOf(e));
+                            }
+
+                        }
+
+                    });
+
+
+                    //tv.setText(bTxt);
 
                 }
             });
-
-
-
         }
+
+
+
+
+
+
+
+
+
+        playBtn = (ImageButton) findViewById(R.id.txtBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Button b = (Button)v;
+                String bTxt = et.getText().toString();
+
+                // change variable globally
+                text = bTxt;
+
+
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        try {
+                            String response = Translate();
+                            //tv.setText(response); //prints stringified json
+                            tv.setText(getTranslatedText(response));
+
+
+                        } catch (Exception e) {
+                            Log.e("TAG", String.valueOf(e));
+                        }
+
+                    }
+
+                });
+
+
+                //tv.setText(bTxt);
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -109,8 +343,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
         NavigationView navigationView1 = findViewById(R.id.lang_select_view);
         navigationView1.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
